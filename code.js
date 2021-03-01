60e7aca7089a4152490222c7697763544f7a9474
gfanou <galleryneil@gmail.com>
2021-02-28 19:59:34 -0600

    Initial committ

---
+++ code.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+    $("button").click(loanDecision);
+    // Object containing the validation rules
+    let myRules =
+        {
+            firstName:
+                {  required: true
+
+                },
+            salary:
+                {   required: true,
+                    min:0,
+                    max:1000000,
+                    digits:true
+                },
+            score:
+                {   required: true,
+                    min:300,
+                    max:850,
+                    digits:true
+                },
+            month:
+                {   required: true,
+                    min:0,
+                    digits:true
+
+                },
+        };
+
+    // Object containing the error messages
+    let myMessages =
+        {
+            firstName:
+                {
+                    required: "Please enter your name",
+                }
+
+        };
+
+    function  loanDecision() {
+
+
+        let name = $("#firstName").val();
+        let mySalary = $("#salary").val();
+        let myScore = $("#score").val();
+        let duration = $("#month").val();
+        let output;
+
+
+        if (mySalary >= 40000 && myScore >= 600) {
+            output = `Hey ${name} ! Congratulation! Your loan is approved`;
+        } else if (mySalary >= 40000 & myScore < 600 && duration >= 13) {
+            output = `Hey ${name} ! Congratulation! Your loan is approved`;
+        } else if (mySalary >= 40000 & myScore < 600 && duration < 13) {
+            output = `Hey ${name} ! we're sorry your loan is denied`;
+        } else if (mySalary < 40000 & myScore >= 600 && duration >= 13) {
+            output = `Hey ${name} ! Congratulation! Your loan is approved`;
+        } else if (mySalary < 40000 & myScore >= 600 && duration < 13) {
+            output = `Hey ${name} ! we're sorry your loan is denied`;
+        } else if (mySalary <1 && myScore <1) {
+            output = `Please fill out each field first.`;
+        } else if (mySalary < 40000 && myScore < 600) {
+            output = `Hey ${name}! We're sorry your loan is denied`;
+        }
+
+
+        $(" #message").text(output);
+
+    }
+
+    // Needs submitHandler, rules, and messages properties
+    $("form").validate(
+        {
+            submitHandler: loanDecision,
+            rules: myRules,
+            messages: myMessages
+        }
+    );
+
+});
